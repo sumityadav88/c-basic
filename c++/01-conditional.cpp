39bcb5af56b7d63c73651548af7951c9a14f180a
sumityadav88 <sumityaadav777@gmail.com>
2024-07-24 10:29:38 +0000

    conditonal

---
+++ c++/01-conditional.cpp
@@ -0,0 +1,32 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter a age: ";
+    cin>>age;
+
+    // if(number % 2 == 0){
+    //     cout<<"Enter number is Even";
+    // }
+    // else{
+    //     cout<<"Enter number is Odd";
+    // }
+
+    // if(age < 12){
+    //     cout<<"you are chile";
+    // }
+    // else if(age > 12 && age < 18){
+    //     cout<<"you are a teenager";
+    // }
+    // else{
+    //     cout<<"Pure Adult, enjoy your beer";
+    // }
+    
+    // ternary operator
+    // age >= 18 ? cout<<"adult"<<endl : cout<<"child";
+
+    
+    
+    return 0;
+}
